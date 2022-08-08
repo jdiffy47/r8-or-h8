6b74361ba67bf3ef8c33c5bdb87c853a2632b791
JakeD <jakediefenderfer@gmail.com>
2022-08-08 12:37:13 -0400

    stub up base.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-
 from django.http import HttpResponse
 
 
@@ -8,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About r8 or h8</h1>')
+  return render(request, 'about.html')
