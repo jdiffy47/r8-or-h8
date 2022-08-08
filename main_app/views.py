1186a7340dc3d27320f147ad34232503475480f0
JakeD <jakediefenderfer@gmail.com>
2022-08-08 14:21:31 -0400

    add Bar model to models.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
+from .models import Bar
 
 
 
-class Bar:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, area, description):
-    self.name = name
-    self.area = area
-    self.description = description
+# class Bar:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, area, description):
+#     self.name = name
+#     self.area = area
+#     self.description = description
 
-bars = [
-  Bar('Tellus360', 'Lancaster PA', '3 story building with rooftop'),
-  Bar('Marion Court', 'Lancaster PA', 'Go here if you want to get shot or kicked out'),
-  Bar('Decades', 'Lancaster PA', 'Gaming bar with arcade and bowling'),
-]
+# bars = [
+#   Bar('Tellus360', 'Lancaster PA', '3 story building with rooftop'),
+#   Bar('Marion Court', 'Lancaster PA', 'Go here if you want to get shot or kicked out'),
+#   Bar('Decades', 'Lancaster PA', 'Gaming bar with arcade and bowling'),
+# ]
 
 
 def home(request):
@@ -22,4 +23,5 @@ def about(request):
   return render(request, 'about.html')
 
 def bars_index(request):
+  bars = Bar.objects.all()
   return render(request, 'bars/index.html', { 'bars': bars })
