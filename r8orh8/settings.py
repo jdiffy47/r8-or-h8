f8d074af5ed0d05d6d2683d289a221393cd51cba
JakeD <jakediefenderfer@gmail.com>
2022-08-11 10:38:38 -0400

    test2

--- r8orh8/settings.py
+++ r8orh8/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(nk3b)75@df9_m0jzntni8oi1s+w$b+z3n*=bpi@r7e86@pn!x'
+# SECRET_KEY = 'django-insecure-(nk3b)75@df9_m0jzntni8oi1s+w$b+z3n*=bpi@r7e86@pn!x'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
